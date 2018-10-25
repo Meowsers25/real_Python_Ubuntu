eb69925b08b6d7075fb89f31f9397f08414f8805
Meowsers25 <c.lamothe25@gmail.com>
2018-10-25 15:08:50 -0400

    A little string work

---
+++ strings/testy.py
@@ -0,0 +1,29 @@
+print("""this is a 
+    string that spans multiple 
+    lines......""")
+print('Here is a string across multiple\
+lines but printed out on one.')
+
+phrase = 'Here is some Py practice'
+print(len(phrase))
+print(phrase[3])
+
+print(phrase[0:5])  # 0 - 5
+print(phrase[:5])  # 0 - 5
+print(phrase[5:])  # 5 - end
+print(phrase[:])  # whole thing
+
+first_name = 'Luna'
+last_name = 'Petunia'
+print(first_name, last_name)
+bazoo = 'bazinga'
+print(bazoo[2:6])  #up to but not including
+
+loud_voice = "Can you hear me now"
+print(loud_voice.upper())
+u_i = input('Hey what\'s up>')
+print(u_i)
+
+response = input('What shouls I yell?')
+response = response.upper()
+print('OK here goes.......', response)
